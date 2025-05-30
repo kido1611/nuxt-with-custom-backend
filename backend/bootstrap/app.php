dc55f6c2f7177553d2568defb0c02bfad29a0ec7
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-05-30 19:20:23 +0700

    chore: force accept json when accessing api routes

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -12,7 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->api(
+            prepend: [
+                \App\Http\Middleware\ForceApiJsonMiddleware::class,
+            ]
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
