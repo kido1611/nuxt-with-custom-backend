b9a2e19cc08849931c17586635aef6e6b021d87e
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-05-31 06:53:21 +0700

    feat(frontend): add dashboard page

---
+++ frontend/app/layouts/dashboard.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <header class="bg-neutral-700 text-white h-14">header</header>
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
