476a04abe870b111d95650c644c166f3d198eee1
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-05-31 07:25:42 +0700

    deps(frontend): add nuxt-auth-sanctum

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,10 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "nuxt-auth-sanctum"],
   css: ["~/assets/css/main.css"],
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
+  },
+  sanctum: {
+    baseUrl: "http://localhost:8000",
+    endpoints: {
+      login: "/api/login",
+      logout: "/api/logout",
+    },
+    redirect: {
+      onLogin: "/dashboard",
+    },
   },
 });
