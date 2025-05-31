dd8b37a19184505499f7fa525c34d27daadda10a
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-05-31 16:01:45 +0700

    chore(frontend): improve dashboard layout

--- frontend/app/pages/dashboard/index.vue
+++ frontend/app/pages/dashboard/index.vue
@@ -8,5 +8,11 @@ definePageMeta({
 <template>
   <div>
     <h1>Dashboard</h1>
+
+    <div>
+      <NuxtLink to="/dashboard/notes" class="font-semibold px-4 py-3"
+        >See Notes</NuxtLink
+      >
+    </div>
   </div>
 </template>
