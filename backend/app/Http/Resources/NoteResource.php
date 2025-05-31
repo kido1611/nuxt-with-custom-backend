ea036f6547776379e28795d2a402f610f1b8028b
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-05-31 15:50:18 +0700

    chore(backend): add note_id in NoteResource

--- backend/app/Http/Resources/NoteResource.php
+++ backend/app/Http/Resources/NoteResource.php
@@ -19,6 +19,7 @@ class NoteResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'description' => $this->description,
             'is_public' => (bool) $this->visible_at,
