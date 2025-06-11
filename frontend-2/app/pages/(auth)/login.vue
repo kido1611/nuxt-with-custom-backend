b65c99e486d12986599bec7bad8b2a293b521bab
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-06-11 13:56:49 +0700

    feat(frontend-2): add another frontend example without module

---
+++ frontend-2/app/pages/(auth)/login.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+import type { FormSubmitEvent, FormError } from "@nuxt/ui";
+import { LoginSchema, type LoginType } from "~/types";
+
+definePageMeta({
+  layout: "auth",
+  middleware: ["sanctum:guest"],
+});
+
+const toast = useToast();
+const form = useTemplateRef("form");
+
+const { login } = useSanctumAuth();
+
+const state = reactive({
+  email: "ahmad@local.host",
+  password: "password",
+});
+
+async function onSubmit(event: FormSubmitEvent<LoginType>) {
+  form.value?.clear();
+
+  try {
+    await login(event.data);
+    // TODO: fix type
+  } catch (error: any) {
+    toast.add({
+      title: "Error",
+      description: error.data.message,
+      color: "error",
+    });
+
+    const errors: FormError[] = parseError(error.data.errors);
+
+    form.value?.setErrors(errors);
+  }
+}
+</script>
+
+<template>
+  <div class="flex flex-col gap-y-8">
+    <h1 class="text-4xl">Login</h1>
+    <UForm
+      ref="form"
+      :schema="LoginSchema"
+      :state
+      @submit="onSubmit"
+      class="flex flex-col gap-y-5"
+    >
+      <UFormField label="Email" required name="email">
+        <UInput
+          type="email"
+          v-model="state.email"
+          required
+          :ui="{
+            root: 'flex',
+          }"
+        />
+      </UFormField>
+
+      <UFormField label="Password" required name="password">
+        <UInput
+          type="password"
+          v-model="state.password"
+          required
+          :ui="{
+            root: 'flex',
+          }"
+        />
+      </UFormField>
+
+      <UButton type="submit" class="self-start">Login</UButton>
+    </UForm>
+    <p class="text-sm">
+      Doesn't have an account?
+      <UButton to="/register" variant="ghost" color="neutral"
+        >Click here!</UButton
+      >
+    </p>
+  </div>
+</template>
---
+++ frontend-2/app/pages/(auth)/register.vue
@@ -0,0 +1,102 @@
+<script setup lang="ts">
+import type { FormSubmitEvent, FormError } from "@nuxt/ui";
+import { RegisterSchema, type RegisterType } from "~/types";
+
+definePageMeta({
+  layout: "auth",
+  middleware: ["sanctum:guest"],
+});
+
+const client = useSanctumClient();
+const toast = useToast();
+const form = useTemplateRef("form");
+
+const state = reactive({
+  name: "ahmad",
+  email: "ahmad@local.host",
+  password: "password",
+});
+
+async function onSubmit(event: FormSubmitEvent<RegisterType>) {
+  form.value?.clear();
+
+  try {
+    await client("/api/auth/register", {
+      method: "POST",
+      body: event.data,
+    });
+
+    await navigateTo("/login");
+
+    toast.add({
+      title: "Success",
+      color: "success",
+    });
+    // TODO: fix type
+  } catch (error: any) {
+    toast.add({
+      title: "Error",
+      description: error.data.message,
+      color: "error",
+    });
+
+    const errors: FormError[] = parseError(error.data.errors);
+
+    form.value?.setErrors(errors);
+  }
+}
+</script>
+
+<template>
+  <div class="flex flex-col gap-y-8">
+    <h1 class="text-4xl">Register</h1>
+
+    <UForm
+      ref="form"
+      :schema="RegisterSchema"
+      :state
+      @submit="onSubmit"
+      class="flex flex-col gap-y-5"
+    >
+      <UFormField label="Name" required name="name">
+        <UInput
+          type="text"
+          v-model="state.name"
+          required
+          :ui="{
+            root: 'flex',
+          }"
+        />
+      </UFormField>
+
+      <UFormField label="Email" required name="email">
+        <UInput
+          type="email"
+          v-model="state.email"
+          required
+          :ui="{
+            root: 'flex',
+          }"
+        />
+      </UFormField>
+
+      <UFormField label="Password" required name="password">
+        <UInput
+          type="password"
+          v-model="state.password"
+          required
+          :ui="{
+            root: 'flex',
+          }"
+        />
+      </UFormField>
+
+      <UButton type="submit" class="self-start">Register</UButton>
+    </UForm>
+
+    <p class="text-sm">
+      Already have an account?
+      <UButton to="/login" variant="ghost" color="neutral">click here!</UButton>
+    </p>
+  </div>
+</template>
